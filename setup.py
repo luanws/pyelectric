8e9c9ba1de0ebc44d57ef6e78eef0595ebeefcc8
luanws <luan.w.silveira@gmail.com>
2021-10-19 22:51:46 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyeletric",
-    version="1.0.0",
+    version="1.0.1",
     author="luanws",
     author_email="luan.w.silveira@gmail.com",
     description="Python package for electric circuits",
