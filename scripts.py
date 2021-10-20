b01658193831817c5c3be8da426a0ef4bb456658
luanws <luan.w.silveira@gmail.com>
2021-10-19 22:23:20 -0300

    first commit

---
+++ scripts.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+from contextlib import suppress
+
+import setuptools
+
+from pymenu import Menu
+
+
+def clear_build():
+    folders = ['build', 'dist', 'pymenu_console.egg-info']
+    for folder in folders:
+        with suppress(FileNotFoundError):
+            shutil.rmtree(folder)
+
+
+def show_packages():
+    print(setuptools.find_packages())
+
+
+def deploy():
+    clear_build()
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    clear_build()
+
+
+menu = Menu('Scripts')
+menu.add_options([
+    ('build', lambda: os.system('python setup.py sdist bdist_wheel')),
+    ('publish', lambda: os.system('twine upload dist/*')),
+    ('clear build', clear_build),
+    ('show packages', show_packages),
+    ('deploy', deploy),
+])
+menu.show()
