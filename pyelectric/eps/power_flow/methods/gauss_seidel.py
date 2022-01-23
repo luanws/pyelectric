9300cbed3b019efd04ccd75710c02a92b38dd064
luanws <luan.w.silveira@gmail.com>
2022-01-23 17:38:17 -0300

    feat(eps): power flow steps

--- pyelectric/eps/power_flow/methods/gauss_seidel.py
+++ pyelectric/eps/power_flow/methods/gauss_seidel.py
@@ -8,6 +8,7 @@ from pyelectric.eps.power_flow.bar.load_bar import LoadBar
 from pyelectric.eps.power_flow.bar.regulator_bar import RegulatorBar
 from pyelectric.eps.power_flow.bar.slack_bar import SlackBar
 from pyelectric.eps.power_flow.circuit import Circuit
+from pyelectric.eps.power_flow.step import Step
 
 
 class GaussSeidel:
@@ -15,18 +16,21 @@ class GaussSeidel:
     __y_bus_array: np.ndarray
     __voltage_array: np.ndarray
     __power_esp_array: np.ndarray
-    __steps: List[sp.Eq] = []
+    __steps: List[Step] = []
+    k: int = 0
 
     def __init__(self, circuit: Circuit):
         self.circuit = circuit
-        self.__y_bus_array = self.get_y_bus_array()
-        self.__voltage_array = self.get_voltage_array()
-        self.__power_esp_array = self.get_power_esp_array()
 
     def __str__(self) -> str:
-        bars = '\n'.join([str(bar) for bar in self.circuit.bars])
-        lines = '\n'.join([str(line) for line in self.circuit.lines])
-        return f'{bars}\n{lines}'
+        return str(self.circuit)
+
+    @property
+    def steps(self):
+        return self.__steps
+
+    def add_step(self, *args, **kwargs):
+        self.__steps.append(Step(*args, **kwargs))
 
     def get_y_bus_array(self) -> np.ndarray:
         y_bus = np.zeros((len(self.circuit.bars), len(
@@ -43,6 +47,7 @@ class GaussSeidel:
             y_bus[bar2_index, bar2_index] = sum(
                 [l.admittance for l in self.circuit.lines if l.bar1 == line.bar2 or l.bar2 == line.bar2])
 
+        self.add_step('Y_bus', sp.Matrix(y_bus))
         return y_bus
 
     def get_power_esp_array(self) -> np.ndarray:
@@ -53,16 +58,21 @@ class GaussSeidel:
                 power_d[i] = bar.power
             elif isinstance(bar, RegulatorBar):
                 power_g[i] = bar.active_power
+            if not isinstance(bar, SlackBar):
+                self.add_step(f'S^esp_{bar.name}', sp.parse_expr(
+                    f'(S_G{bar.name} - S_D{bar.name})/S_base'))
+                self.add_step(f'S^esp_{bar.name}', power_g[i] - power_d[i])
         power_esp = power_g - power_d
         return power_esp
 
-    def get_voltage_array(self) -> np.ndarray:
+    def get_initial_voltage_array(self) -> np.ndarray:
         voltages = np.ones(len(self.circuit.bars), dtype=complex)
         for i, bar in enumerate(self.circuit.bars):
             if isinstance(bar, SlackBar):
                 voltages[i] = bar.voltage
             elif isinstance(bar, RegulatorBar):
                 voltages[i] = bar.voltage_module
+            self.add_step(f'V^0_{bar.name}', voltages[i])
         return voltages
 
     def get_bar_power(self, bar: Bar) -> complex:
@@ -87,6 +97,10 @@ class GaussSeidel:
         return V
 
     def solve(self, repeat: int = 1, max_error: float = None):
+        self.__steps = []
+        self.__y_bus_array = self.get_y_bus_array()
+        self.__voltage_array = self.get_initial_voltage_array()
+        self.__power_esp_array = self.get_power_esp_array()
         if max_error is None:
             for _ in range(repeat):
                 self.__update_bar_voltages()
@@ -104,6 +118,18 @@ class GaussSeidel:
         self.__update_line_amperages()
         self.__update_line_powers()
 
+    def get_voltage_expression(self, bar: Bar):
+        i = self.circuit.get_bar_index(bar)
+        voltage_array = self.__voltage_array
+        S_symbol = sp.Symbol(f'S^esp_{bar.name}')
+        V_symbol = sp.Symbol(f'V^({self.k})_{bar.name}')
+        I_symbol = S_symbol.conjugate()/V_symbol
+        summation_symbol = sum([sp.Symbol(
+            f'Y_{bar.name}_{j+1}')*sp.Symbol(f'V^(k)_{j+1}') for j in range(len(voltage_array)) if i != j])
+        Y_symbol = sp.Symbol(f'Y_{i+1}_{i+1}')
+        V_expression = (I_symbol - (summation_symbol))/Y_symbol
+        return V_expression
+
     def __update_bar_voltages(self):
         voltage_array = self.__voltage_array
         power_esp = self.__power_esp_array
@@ -111,6 +137,10 @@ class GaussSeidel:
         for i, bar in enumerate(self.circuit.bars):
             if isinstance(bar, LoadBar):
                 voltage_array[i] = self.get_bar_voltage(bar)
+
+                voltage_expression = self.get_voltage_expression(bar)
+                self.add_step(f'V^({self.k + 1})_{bar.name}', voltage_expression)
+                self.add_step(f'V^({self.k + 1})_{bar.name}', voltage_array[i])
             elif isinstance(bar, RegulatorBar):
                 Q = self.get_bar_power(bar).imag
                 P = power_esp[i].real
@@ -119,7 +149,17 @@ class GaussSeidel:
                 V_real = math.sqrt(bar.voltage_module**2 - V.imag**2)
                 voltage_array[i] = V_real + V.imag*1j
 
+                voltage_expression = self.get_voltage_expression(bar)
+                imag_voltage = sp.im(voltage_expression, evaluate=False)
+                voltage_symbol = sp.Symbol(f'V^({self.k + 1})_{bar.name}')
+                voltage_module_symbol = sp.Symbol(f'|{"{"}V_{bar.name}{"}"}|')
+                self.add_step(sp.im(voltage_symbol), imag_voltage)
+                real_voltage = sp.sqrt(voltage_module_symbol**2 - sp.im(voltage_symbol)**2, evaluate=False)
+                self.add_step(sp.re(voltage_symbol), real_voltage)
+                self.add_step(voltage_symbol, voltage_array[i])
+
         self.__voltage_array = voltage_array
+        self.k += 1
         self.circuit.update_bar_voltages(voltage_array)
 
     def __update_bar_powers(self):
