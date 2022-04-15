fa626614ca7500184793148f0d8d07a425d6066b
luanws <luan.w.silveira@gmail.com>
2022-04-15 15:15:42 -0300

    feat(eps): fortescue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyelectric",
-    version="0.4.7",
+    version="0.5.0",
     author="luanws",
     author_email="luan.w.silveira@gmail.com",
     description="Python package for electric circuits",
