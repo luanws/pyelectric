6c1b1b06dbc17426623c6c81eb8bdff78e2a9333
luanws <luan.w.silveira@gmail.com>
2022-04-15 11:51:47 -0300

    feat(utils/numerize): format complex

--- pyelectric/utils/numerize.py
+++ pyelectric/utils/numerize.py
@@ -15,13 +15,26 @@ def ends_with_suffix(number_str: str, suffixes: Union[str, List[str]]) -> bool:
     return False
 
 
-def get_suffix(suffix_options: Union[str, List[str]]) -> str:
+def get_suffix_from_number(number: float) -> str:
+    suffixes = positive_suffixes + negative_suffixes[::-1]
+    if number == 0:
+        return ''
+    magnitude = int(math.floor(math.log(abs(number), 1e3)))
+    if magnitude <= len(positive_suffixes) - 1 and magnitude >= -len(negative_suffixes):
+        return get_suffix_from_options(suffixes[magnitude])
+    return ''
+
+
+def get_suffix_from_options(suffix_options: Union[str, List[str]]) -> str:
     if isinstance(suffix_options, str):
         return suffix_options
     return suffix_options[0]
 
 
-def format(number: float, unit='', decimal: int = 2) -> str:
+def format(number: complex, unit='', decimal: int = 2) -> str:
+    if isinstance(number, complex):
+        return format_complex(number, unit, decimal)
+
     suffixes = positive_suffixes + negative_suffixes[::-1]
     if number == 0:
         return '0'
@@ -48,5 +61,21 @@ def revert(number_str: str, unit: str = '') -> float:
             sorted_sufixes = negative_suffixes[::-1] + positive_suffixes
             magnitude = sorted_sufixes.index(suffix_options) - len(negative_suffixes)
             if magnitude != 0:
-                return float(number_str[:-len(get_suffix(suffix_options))]) * 1e3**magnitude
+                return float(number_str[:-len(get_suffix_from_options(suffix_options))]) * 1e3**magnitude
     return float(number_str)
+
+
+def format_complex(number: complex, unit: str = '', decimal: int = 2) -> str:
+    if number.imag == 0:
+        return format(number.real, unit, decimal)
+    if number.real == 0:
+        return 'j' + format(number.imag, unit, decimal)
+    real = format(number.real, '', decimal)
+    imag = format(number.imag, '', decimal)
+    real_suffix = get_suffix_from_number(number.real)
+    imag_suffix = get_suffix_from_number(number.imag)
+    if real_suffix == imag_suffix:
+        real = real.replace(real_suffix, '')
+        imag = imag.replace(imag_suffix, '')
+        return f'({real} + {imag}j){real_suffix}{unit}'
+    return f'({real} + {imag}j){unit}'
